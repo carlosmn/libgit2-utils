4b58ff9028c42f9be311df16793c2cba21e4f4c1
Carlos Martín Nieto <carlos@cmartin.tk>
2011-06-07 11:06:14 +0200

    parse-pkt-line: parse multiple lines

--- parse-pkt-line.c
+++ parse-pkt-line.c
@@ -1,28 +1,35 @@
 #include <git2.h>
+#include <stdio.h>
 #include "common.h"
 
 int parse_pkt_line(git_repository *repo, int argc, char **argv)
 {
   char *line = argv[1];
-  char *after, oid[GIT_OID_HEXSZ+1] = {0};
+  char *after = line, oid[GIT_OID_HEXSZ+1] = {0};
   int error;
   git_pkt *pkt;
 
-  error = git_pkt_parse_line(&pkt, line, &after);
-  if (error < GIT_SUCCESS)
-    return error;
+  while(*after != '\0'){
+	  error = git_pkt_parse_line(&pkt, line, &after);
+	  if (error < GIT_SUCCESS)
+		  return error;
 
-  switch(pkt->type){
-  case GIT_PKT_REF:
-    {
-      git_remote_head *h = &(((git_pkt_ref *)pkt)->head);
-      puts("Found a ref pkt");
-      git_oid_fmt(oid, &h->oid);
-      printf("%s\t%s\n", oid, h->name);
-      break;
-    }
-  default:
-    return GIT_EOBJCORRUPTED;
+	  line = after;
+
+	  switch(pkt->type){
+	  case GIT_PKT_REF:
+		  {
+			  git_remote_head *h = &(((git_pkt_ref *)pkt)->head);
+			  puts("Found a ref pkt");
+			  git_oid_fmt(oid, &h->oid);
+			  printf("%s\t%s\n", oid, h->name);
+			  break;
+		  }
+	  case GIT_PKT_FLUSH:
+		  puts("flush! do something!");
+	  default:
+		  printf("default: %d\n", *after);
+	  }
   }
  
   return error;
