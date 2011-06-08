40f909efe5a738d2674c0a8df92ae47fecb43cfa
Carlos Martín Nieto <carlos@cmartin.tk>
2011-06-08 11:32:02 +0200

    Use stdin and show caps
    Signed-off-by: Carlos Martín Nieto <carlos@cmartin.tk>

--- parse-pkt-line.c
+++ parse-pkt-line.c
@@ -1,13 +1,29 @@
 #include <git2.h>
 #include <stdio.h>
+#include <string.h>
 #include "common.h"
 
 int parse_pkt_line(git_repository *repo, int argc, char **argv)
 {
   char *line = argv[1];
   char *after = line, oid[GIT_OID_HEXSZ+1] = {0};
-  int error;
+  int error, len;
   git_pkt *pkt;
+
+  /*
+   * Assume that the user has piped a file in
+   */
+  fseek(stdin, 0, SEEK_END);
+  len = ftell(stdin);
+  fseek(stdin, 0, SEEK_SET);
+
+  line = malloc(len+1);
+  if(line == NULL)
+    return GIT_ENOMEM;
+
+  memset(line, 0, len+1);
+  fread(line, len, sizeof(char), stdin);
+  after = line;
 
   while(*after != '\0'){
 	  error = git_pkt_parse_line(&pkt, line, &after);
@@ -20,9 +36,12 @@ int parse_pkt_line(git_repository *repo, int argc, char **argv)
 	  case GIT_PKT_REF:
 		  {
 			  git_remote_head *h = &(((git_pkt_ref *)pkt)->head);
-			  puts("Found a ref pkt");
+			  const char *caps = ((git_pkt_ref *)pkt)->capabilities;
+			  //puts("Found a ref pkt");
 			  git_oid_fmt(oid, &h->oid);
 			  printf("%s\t%s\n", oid, h->name);
+			  if(caps != NULL)
+			    printf("    Capabilities: %s\n", caps);
 			  break;
 		  }
 	  case GIT_PKT_FLUSH:
